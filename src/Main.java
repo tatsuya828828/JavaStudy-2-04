a4febecf3aa6a0c4f71bed307b4d133de1d7484c
tatsuya828828 <kkjy011894@gmail.com>
2020-07-14 10:45:46 +0900

    [start]JavaStudy-2-04

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import job.Hero;
+
+public class Main {
+	public static void main(String[] args) {
+		Hero hero = new Hero("たかし");
+		String  h = hero.toString();
+		System.out.println(h);
+	}
+}
---
+++ src/job/Hero.java
@@ -0,0 +1,22 @@
+package job;
+
+public class Hero {
+	private String name;
+	private int hp, mp;
+
+	public Hero(String name) {
+		this.name = name;
+		this.hp = 100;
+		this.mp = 50;
+	}
+
+	public Hero() {
+		this("ひろし");
+	}
+
+	// オーバーライドしなければObjectクラスで定義されたtoStringが呼び出されるため意味のわからない文字列が返ってくる
+	// そのためオーバーライドして自分が表示したい内容を定義する
+	public String toString() {
+		return "名前:"+ this.name +"/HP:"+ this.hp +"/MP:"+ this.mp;
+	}
+}
